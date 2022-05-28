db10dbc3e000d4574f4e8b5043b84309a29ed9e2
ae_jguzman <ae_jguzman@idcontractors.net>
2022-05-28 01:53:45 -0500

    Improve algorithm

--- index.js
+++ index.js
@@ -9,62 +9,111 @@ const getEMA = async (client, symbol, interval, limit) => {
     return ema(closeTicks, limit)
 }
 
-const tick = async(config, client, clientExt) => {
-    const { asset, base, amount } = config;
+const getAmount = async (clientExt, markPrice) => {
+    const balance = await clientExt.futures.balance()
+    const BUSDAV = balance.BUSD.available
+    const amountFlot = (BUSDAV / Number(markPrice)) - ((BUSDAV / Number(markPrice)) / 10)
+    return amountFlot.toFixed(2)
+}
+
+const tick = async (config, client, clientExt) => {
+    const { asset, base } = config;
     const symbol = `${asset}${base}`
     try {
-        const ticker = await client.futuresMarkPrice(symbol)
-        const ema85 = await getEMA(client, symbol, '1m', 85)
-        const ema3 = await getEMA(client, symbol, '1m', 3)
-        const openOrders = await clientExt.futures.openOrders(symbol)
-        console.log(openOrders)
-        if(ema3 >= ema85) {
-            console.log('COMPRA')
-            if (openOrders.length === 0) {
-                // No hay posiciones, abre una nueva
-                await clientExt.futures.marketBuy(symbol, amount)
-                // Abre orden de compra ghost
-                await clientExt.futures.buy(symbol, 1, 10)
-            } else {
-                // Si ya existe una orden de compra la liquida
-                if (openOrders[0].side !== 'BUY') {
-                    await clientExt.futures.cancelAll(symbol)
-                    await clientExt.futures.marketSell(obj.symbol, amount, {
-                        reduceOnly: true,
-                    });
+        const markPriceResponse = await clientExt.futures.markPrice(symbol)
+        const markPrice = markPriceResponse.indexPrice
+        const markPriceFixed = Number(markPrice).toFixed(2)
+        const minBUSDToTrade = 0.51
+        if (await getAmount(clientExt, markPrice) > 0) {
+            const ema85 = await getEMA(client, symbol, '1m', 40)
+            const ema1 = await getEMA(client, symbol, '1m', 1)
+            const openOrders = await clientExt.futures.openOrders(symbol)
+            let amount = await getAmount(clientExt, markPrice)
+            if (ema1 >= ema85) {
+                if (openOrders.length === 0) {
+                    console.log('Creando LONG Ghost...')
+                    await clientExt.futures.buy(symbol, minBUSDToTrade, 10)
+                    console.log('Ghost creado exitosamente')
+                    // No hay posiciones, abre una nueva
+                    console.log('Ejecutando LONG...')
                     await clientExt.futures.marketBuy(symbol, amount)
-                    await clientExt.futures.buy(symbol, 1, 10)
+                    console.log('LONG inicial exitoso')
+                    console.log('###################################')
+                } else {
+                    // Si ya existe una orden de compra la liquida
+                    if (openOrders[0].side !== 'BUY') {
+                        console.log('Intentando liquidar ordenes SHORT...')
+                        try {
+                            await clientExt.futures.marketBuy(symbol, amount, {
+                                reduceOnly: true,
+                            });
+                        } catch {
+                            await clientExt.futures.marketSell(symbol, amount, {
+                                reduceOnly: true,
+                            });
+                        }
+                        console.log('Ordenes SHORT liquidadas')
+                        console.log('Cancelando ordenes ghost...')
+                        await clientExt.futures.cancelAll(symbol)
+                        console.log('Ghosts cancelados')
+                        console.log('Creando Ghost...')
+                        await clientExt.futures.buy(symbol, minBUSDToTrade, 10)
+                        console.log('Ghost LONG creado exitosamente')
+                        amount = await getAmount(clientExt, markPrice)
+                        console.log('Ejecutando LONG...')
+                        await clientExt.futures.marketBuy(symbol, amount)
+                        console.log('LONG exitoso')
+                        console.log('###################################')
+                    }
                 }
-            }
-        } else {
-            console.log('VENTA')
-            if (openOrders.length === 0) {
-                await clientExt.futures.marketSell(symbol, amount)
-                await clientExt.futures.sell(symbol, 0.02, ticker * 10000)
             } else {
-                if (openOrders[0].side !== 'SELL') {
-                    await clientExt.futures.cancelAll(symbol)
-                    await clientExt.futures.marketBuy(obj.symbol, amount, {
-                        reduceOnly: true,
-                    });
+                if (openOrders.length === 0) {
+                    console.log('Creando SHORT Ghost...')
+                    await clientExt.futures.sell(symbol, 0.01, markPriceFixed * 2)
+                    console.log('Ghost creado exitosamente')
+                    console.log('Ejecutando SHORT...')
                     await clientExt.futures.marketSell(symbol, amount)
-                    await clientExt.futures.sell(symbol, 0.02, ticker * 10000)
+                    console.log('SHORT inicial exitoso')
+                    console.log('###################################')
+                } else {
+                    if (openOrders[0].side !== 'SELL') {
+                        console.log('Intentando liquidar ordenes LONG...')
+                        try {
+                            await clientExt.futures.marketSell(symbol, amount, {
+                                reduceOnly: true,
+                            });
+                        } catch {
+                            await clientExt.futures.marketBuy(symbol, amount, {
+                                reduceOnly: true,
+                            });
+                        }
+                        console.log('Ordenes LONG liquidadas')
+                        console.log('Cancelando ordenes ghost...')
+                        await clientExt.futures.cancelAll(symbol)
+                        console.log('Ghosts cancelados')
+                        console.log('Creando SHORT Ghost...')
+                        await clientExt.futures.sell(symbol, 0.01, markPriceFixed * 2)
+                        console.log('Ghost creado exitosamente')
+                        amount = await getAmount(clientExt, markPrice)
+                        console.log('Ejecutando SHORT...')
+                        await clientExt.futures.marketSell(symbol, amount)
+                        console.log('SHORT exitoso')
+                        console.log('###################################')
+                    }
                 }
             }
         }
+
     } catch (e) {
         console.info('Error', e)
     }
-    
 }
 
 const run = () => {
     const config = {
         asset: 'BNB',
         base: 'BUSD',
-        allocation: 0.1,
-        tickInterval: 20000,
-        amount: 0.04
+        tickInterval: 5000
     }
 
     const binanceClient = Binance({
